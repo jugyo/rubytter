28528d61370e96d6a6f3a5534996ecabcac64818
jugyo <jugyo.org@gmail.com>
2009-02-14 09:01:09 +0900

    add a example

---
+++ examples/favorite.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'rubytter'
+
+if ARGV.size < 3
+  puts "Usage: ruby #{File.basename(__FILE__)} user_id password id"
+  exit
+end
+
+client = Rubytter.new(ARGV[0], ARGV[1])
+client.favorite(ARGV[2])
