e848ec6f67865f86752ad7fec390ecadd0b9099f
jugyo <jugyo.org@gmail.com>
2009-10-31 10:14:17 +0900

    add example for using Lists API

---
+++ examples/lists.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'rubytter'
+
+if ARGV.size < 2
+  puts "Usage: ruby #{File.basename(__FILE__)} user_id password"
+  exit
+end
+
+client = Rubytter.new(ARGV[0], ARGV[1])
+
+puts 'create list...'
+list = client.create_list('test')
+puts "  => : #{list.full_name}"
+
+puts 'get lists..'
+puts '  =>' + client.lists(ARGV[0]).lists.map{|i| i.slug}.inspect
+
+puts 'add member to list...'
+add_user = client.user('termtter')
+client.add_member_to_list(list.slug, add_user.id)
+members = client.list_members(ARGV[0], list.slug)
+puts '  =>' + members.users.map{|i| i.screen_name}.inspect
+
+puts 'remove member from list...'
+client.remove_member_from_list(list.slug, add_user.id)
+members = client.list_members(ARGV[0], list.slug)
+puts '  =>' + members.users.map{|i| i.screen_name}.inspect
+
+puts 'delete list...'
+client.delete_list(list.slug)
+
+puts 'get lists..'
+puts '  =>' + client.lists(ARGV[0]).lists.map{|i| i.slug}.inspect
+
+puts 'done'
