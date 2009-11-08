18383ecd8262386652e6ed10ea10bbba00e91181
seisuke <seisuke@ymail.com>
2009-11-08 20:58:24 +0900

    OAuth経由のAPI利用時に,deleteとputを追加

--- lib/rubytter/oauth_rubytter.rb
+++ lib/rubytter/oauth_rubytter.rb
@@ -19,6 +19,18 @@ class OAuthRubytter < Rubytter
     structize(@access_token.post(path, params, @header))
   end
 
+  def put(path, params = {})
+    path += '.json'
+    structize(@access_token.put(path, params, @header))
+  end
+
+  def delete(path, params = {})
+    path += '.json'
+    param_str = self.class.to_param_str(params)
+    path = path + '?' + param_str unless param_str.empty?
+    structize(@access_token.delete(path, @header))
+  end
+
   def structize(res)
     json_data = JSON.parse(res.body)
     case res.code
